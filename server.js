529eb604c7fb649e7afec30a6f32e9fbbc68134e
Sarveshwar Mahapatro <f20190257@goa.bits-pilani.ac.in>
2021-08-03 17:39:17 +0530

    get data on server

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const path = require('path')
+
 const bodyParser = require('body-parser')
 
 const app=express()
 app.use('/',express.static(path.join(__dirname,'static')))
 
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 
 
 app.post('/api/register', async (req, res) => {
 	console.log(req.body)
-    res.json({status: 'OK'})
+    res.json({status:'ok'})
 })
 
 app.listen(3000, () => {
