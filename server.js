930fd168790d8fc48eaebe9ff645464adcdcce91
Sarveshwar Mahapatro <f20190257@goa.bits-pilani.ac.in>
2021-08-03 18:02:54 +0530

    register user

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 
-const user = require('./model/user')
+const User = require('./model/user')
+
+const bcrypt= require('bcryptjs')
 
 mongoose.connect('mongodb://localhost:27017/login-app-db', {
 	useNewUrlParser: true,
@@ -18,7 +20,13 @@ app.use(bodyParser.json())
 
 
 app.post('/api/register', async (req, res) => {
-	console.log(req.body)
+
+    const { username, password: plainTextPassword } = req.body
+
+    const password = await bcrypt.hash(plainTextPassword, 10)
+    
+    
+    console.log(req.body)
     res.json({status:'ok'})
 })
 
